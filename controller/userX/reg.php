cf17d44205a566162d0bee503293c24bc410349a
Sebastianj123 <113195211+Sebastianj123@users.noreply.github.com>
2023-08-25 21:42:43 -0500

    .

--- controller/userX/reg.php
+++ controller/userX/reg.php
@@ -16,6 +16,16 @@
   $per_addr = $_REQUEST['per_addr'];
   $pass = $_REQUEST['pass'];
 
+  $sql = "CALL v_user_repeat('$user_name','$user_ema',$user_tel,$per_doc);";
+  $query = $con -> query($sql);
+  $queryAssoc = $query -> fetch_all(MYSQLI_ASSOC);
+
+  if (isset($queryAssoc)) {
+    $sql1 = "CALL i_client('$per_name','$per_lastName',$sex_id,$tyDoc_id,$per_doc,'$per_bith','$user_name','$user_ema',$user_tel,'$user_pass','$per_addr');";
+    $query1 = $con -> query($sql1); 
+  }
+
+
 
   
     // $name = $per_name.' '.$lastName;
